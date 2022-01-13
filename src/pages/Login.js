5a59db99210c9e6b51c4f3e6fabebd28bf00c788
Baran Öden <baranoden@gmail.com>
2022-01-13 17:33:49 +0300

    final

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,10 +14,14 @@ function Login({ auth }) {
       e.name === username ? e : null
     );
 
-    if (user[0]?.password === pass) {
-      return navigate("todo") & auth();
+    if (user.length === 0) {
+      alert("no match");
     } else {
-      return alert("Wrong Password");
+      if (user[0]?.password === pass) {
+        return navigate("todo") & auth();
+      } else {
+        return alert("Wrong Password");
+      }
     }
   };
 
